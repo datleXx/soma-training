605f7d590b5c3329652d84735a5e736242a3bcc5
datleXx <129921351+datleXx@users.noreply.github.com>
2024-08-12 14:11:42 +1000

    minor fix on auth.ts

--- src/server/api/routers/companies.ts
+++ src/server/api/routers/companies.ts
@@ -8,36 +8,40 @@ import {
 } from "~/server/api/trpc";
 
 export const companiesRouter = createTRPCRouter({
-    fetchTenCompanies: protectedProcedure
-    .query(async ({ ctx }) => {
-        try {
-
-            const companies = await ctx.db.company.findMany({
-                take: 10,
-                include: {
-                    sectors: true
-                }
-            })
-            return companies;
-        }
-        catch (e) {
-            console.log(e);
-        }
-    }),
-    fetchMultipleCompanies: protectedProcedure
-    .query(async ({ ctx }) => {
-        try {
-
-            const companies = await ctx.db.company.findMany({
-                take: 20,
-                include: {
-                    sectors: true
-                }
-            })
-            return companies;
-        }
-        catch (e) {
-            console.log(e);
-        }
-    }),
+  fetchTenCompanies: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const companies = await ctx.db.company.findMany({
+        take: 10,
+        include: {
+          sectors: true,
+        },
+      });
+      return companies;
+    } catch (e) {
+      console.log(e);
+    }
+  }),
+  fetchMultipleCompanies: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const companies = await ctx.db.company.findMany({
+        take: 20,
+        include: {
+          sectors: true,
+        },
+      });
+      return companies;
+    } catch (e) {
+      console.log(e);
+    }
+  }),
+  fetchCompaniesWithInfiniteScroll: protectedProcedure.input(z.object({
+    cursor: z.string().nullish(),
+    limit: z.number().min(10).max(30)
+  }))
+  .query(async ({ ctx, input }) => {
+    const companies = await ctx.db.company.findMany({
+      take: input.limit + 1,
+      cursor: input.cursor ? { id: input.cursor } : undefined,
+    });
+  }),
 });
