92738ab19391894060ffb7491db3958c4738b931
datleXx <129921351+datleXx@users.noreply.github.com>
2024-08-13 12:49:17 +1000

    Fix: Dat - deployment issue at company page

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,10 +1,13 @@
 import CompaniesPage from "../_components/companies/companies-page/companies-page";
+import { Suspense } from "react";
 
 export default async function Companies() {
 
   return (
     <div className="">
+      <Suspense>
         <CompaniesPage />
+      </Suspense>
     </div>
   );
 }
